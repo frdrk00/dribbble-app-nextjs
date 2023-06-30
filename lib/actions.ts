cad0da9d600526a3186f2a6d3901ed09985399b0
frdrk00 <masterkooo@gmail.com>
2023-06-30 22:39:16 +0200

    Displaying posts 1

--- lib/actions.ts
+++ lib/actions.ts
@@ -3,6 +3,7 @@ import {
   createProjectMutation,
   createUserMutation,
   getUserQuery,
+  projectsQuery,
 } from '@/graphql'
 import { GraphQLClient } from 'graphql-request'
 
@@ -88,4 +89,13 @@ export const createNewProject = async (
 
     return makeGraphQLRequest(createProjectMutation, variables)
   }
+}
+
+export const fetchAllProjects = async (
+  category?: string,
+  endcursor?: string
+) => {
+  client.setHeader('x-api-key', apiKey)
+
+  return makeGraphQLRequest(projectsQuery, { category, endcursor })
 }
